0264ead69bad92f4e0c24e35a5cd19bf0ab953e3
jackypy <jacqueline.parra7@gmail.com>
2019-04-21 23:05:10 -0400

    filtrando_un_hash

---
+++ filtrando_un_hash.rb
@@ -0,0 +1,19 @@
+ventas = {
+  Octubre: 65000,
+  Noviembre: 68000,
+  Diciembre: 72000
+}
+
+def filter(hash)
+  filtered_hash = {}
+  hash.each do |k,v|
+    if v < 70000
+      filtered_hash[k] = v
+    end
+  end
+  return filtered_hash
+
+
+end
+
+puts filter(ventas)
